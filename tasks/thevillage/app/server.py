58689e69cdf113dea972f7c45a6acd0769c9c2fb
Vanya Klimenko <k60+git@fmap.me>
2021-02-27 21:24:00 +0300

    thevillage: correct salt size (16 → 12)

--- tasks/thevillage/app/server.py
+++ tasks/thevillage/app/server.py
@@ -33,7 +33,7 @@ PREFIX = "ugra_come_for_tasks_stay_for_"
 SECRET2 = b"GfkGCGfrk5l6S521koyfjR24fkFsdufgEndEFlfPvFwJR"
 COOKIE_SECRET = b"bG9va2tlZWtvbWFhYWFnYWZqZmpma2ZrZmthYWFhZAo="
 SALT1_SIZE = 16
-SALT2_SIZE = 16
+SALT2_SIZE = 12
 
 PROHIBITED_INVITE = 313337
 
