df6ba88fe56c951f1f1630780584c51ccb5f5951
Nikita Sychev <root@nsychev.ru>
2021-02-27 16:07:21 +0300

    nothingtosee: rework

--- tasks/nothingtosee/generate.py
+++ tasks/nothingtosee/generate.py
@@ -3,25 +3,25 @@
 import hmac
 import sys
 import json
-import OpenSSL
-from OpenSSL import crypto
-import os
-import tempfile
 
 PREFIX = 'ugra_v1_p0sm0tr3l1_'
-SECRET1 = b"71A4f8fdE203ab7D"
-SALT1_SIZE = 16
-SECRET2 = b'810adD012KSDAl10'
-SALT2_SIZE = 12
+TOKEN_SECRET = b"71A4f8fdE203ab7D"
+TOKEN_SALT_SIZE = 16
+FLAG_SECRET = b'810adD012KSDAl10'
+FLAG_SALT_SIZE = 12
 
 
-def get_user_tokens():
+def get_token():
     user_id = sys.argv[1]
 
-    token = hmac.new(SECRET1, str(user_id).encode(), 'sha256').hexdigest()[:SALT1_SIZE]
-    flag = PREFIX + hmac.new(SECRET2, token.encode(), 'sha256').hexdigest()[:SALT2_SIZE]
+    token1 = hmac.new(TOKEN_SECRET, str(user_id).encode(), "sha256").hexdigest()[:TOKEN_SALT_SIZE]
+    token2 = hmac.new(TOKEN_SECRET, token1.encode(), "sha256").hexdigest()[:TOKEN_SALT_SIZE]
 
-    return token, flag
+    return token1 + token2
+
+
+def get_flag(token):
+    return PREFIX + hmac.new(FLAG_SECRET, token.encode(), "sha256").hexdigest()[:FLAG_SALT_SIZE]
 
 
 def generate():
@@ -29,7 +29,8 @@ def generate():
         print('Usage: generate.py user_id target_dir', file=sys.stderr)
         sys.exit(1)
 
-    token, flag = get_user_tokens()
+    token = get_token()
+    flag = get_flag(token)
 
     json.dump({
         'flags': [flag],
