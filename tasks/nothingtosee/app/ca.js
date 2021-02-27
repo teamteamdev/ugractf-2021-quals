40b005134f583da42b3eb08f7edbe3ae0af72217
Nikita Sychev <root@nsychev.ru>
2021-02-27 16:07:21 +0300

    nothingtosee: rework

---
+++ tasks/nothingtosee/app/ca.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const crypto = require('crypto');
+const forge = require('node-forge').pki;
+const fs = require('fs');
+const path = require('path');
+
+const keys = forge.rsa.generateKeyPair(2048);
+const cert = forge.createCertificate();
+cert.publicKey = keys.publicKey;
+cert.validity.notBefore = new Date('2021-02-26T08:37:00Z');
+cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 10);
+cert.serialNumber = Math.floor(Math.random() * 281474976711000).toString(16).padStart(14, '0');
+const attrs = [
+    {shortName: 'C', value: 'RU'},
+    {shortName: 'ST', value: 'Ugra'},
+    {shortName: 'L', value: 'Khanty-Mansiysk'},
+    {shortName: 'O', value: '[team Team]'},
+    {shortName: 'OU', value: 'NOC'},
+    {shortName: 'CN', value: '[team Team] Certificate Authority'}
+];
+cert.setSubject(attrs);
+cert.setIssuer(attrs);
+cert.setExtensions([
+    {name: 'basicConstraints', cA: true},
+    {name: 'keyUsage', keyCertSign: true, keyEncipherment: true},
+    {name: 'extKeyUsage', serverAuth: true},
+    {name: 'nsCertType', server: true, sslCA: true}
+])
+cert.sign(keys.privateKey);
+
+const certPem = forge.certificateToPem(cert);
+const keyPem = forge.privateKeyToPem(keys.privateKey);
+
+fs.writeFileSync('root.key', keyPem);
+fs.writeFileSync('root.crt', certPem);
--- tasks/nothingtosee/app/server.js
+++ tasks/nothingtosee/app/server.js
@@ -7,67 +7,115 @@ const fs = require('fs');
 const path = require('path');
 const tls = require('tls');
 
-const STATE_DIR = process.argv[2] ? process.argv[2] : __dirname
+const DOMAIN = 'nothing.q.2021.ugractf.ru';
+const BASE_DIR = __dirname;
+const STATE_DIR = process.argv[2] ? process.argv[2] : __dirname;
 const PREFIX = 'ugra_v1_p0sm0tr3l1_';
-const SECRET2 = Buffer.from('810adD012KSDAl10');
-const SALT2_SIZE = 12;
+const TOKEN_SECRET = Buffer.from("71A4f8fdE203ab7D");
+const TOKEN_SALT_SIZE = 16;
+const FLAG_SECRET = Buffer.from('810adD012KSDAl10');
+const FLAG_SALT_SIZE = 12;
 
-const generateDefaultCert = () => {
-    return generateCert(undefined, true);
+const ROOT_KEY = fs.readFileSync(path.join(BASE_DIR, 'root.key'));
+const ROOT_CERT = fs.readFileSync(path.join(BASE_DIR, 'root.crt'))
+const DH_PARAM = fs.readFileSync(path.join(BASE_DIR, 'dh.pem'))
+
+
+const verifyToken = (token) => {
+    const realPart = token.slice(0, -TOKEN_SALT_SIZE);
+    const hashedPart = token.slice(-TOKEN_SALT_SIZE);
+    const expectedHash = crypto
+                          .createHmac('sha256', TOKEN_SECRET)
+                          .update(Buffer.from(realPart))
+                         .digest('hex')
+                         .slice(0, TOKEN_SALT_SIZE);
+    return hashedPart == expectedHash;
 }
 
-const generateCert = (token, isDefault) => {
-    const certPath = path.join(STATE_DIR, isDefault ? 'default.cert' : token + '.cert');
-    const keyPath = path.join(STATE_DIR, isDefault ? 'default.key' : token + '.key');
 
-    const flag = isDefault ? ''
-        : PREFIX + crypto.createHmac('sha256', SECRET2).update(Buffer.from(token)).digest('hex').slice(0, SALT2_SIZE);
+const generateCert = (token) => {
+    const rootKey = forge.privateKeyFromPem(ROOT_KEY);
+    const rootCert = forge.certificateFromPem(ROOT_CERT);
+
+    const certPath = path.join(STATE_DIR, `${token}.crt`);
+    const keyPath = path.join(STATE_DIR, `${token}.key`);
 
     if (fs.existsSync(certPath) && fs.existsSync(keyPath))
         return { key: fs.readFileSync(keyPath), cert: fs.readFileSync(certPath) };
 
-    const keys = forge.rsa.generateKeyPair(1024);
+    const keys = forge.rsa.generateKeyPair(2048);
     const cert = forge.createCertificate();
     cert.publicKey = keys.publicKey;
     cert.validity.notBefore = new Date();
-    cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 10);
-    cert.serialNumber = Math.floor(Math.random() * 281474976711000).toString(16)
-    const attrs = [{
-        shortName: 'C', value: 'RU',
-    }, {
-        shortName: 'ST', value: 'Ugra',
-    }, {
-        shortName: 'L', value: 'Khanty-Mansiysk',
-    }, {
-        shortName: 'O', value: '[team Team]',
-    }, {
-        shortName: 'OU', value: 'Team',
-    }, {
-        shortName: 'CN', value: isDefault ? "Flag isn't here." : flag,
-    }];
+    cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
+    cert.serialNumber = Math.floor(Math.random() * 281474976711000).toString(16).padStart(14, '0');
+    const attrs = [
+        {shortName: 'C', value: 'RU'},
+        {shortName: 'ST', value: 'Ugra'},
+        {shortName: 'L', value: 'Khanty-Mansiysk'},
+        {shortName: 'O', value: '[team Team]'},
+        {shortName: 'OU', value: 'NOC'},
+        {shortName: 'CN', value: token === 'default' ? `localhost` : `${token}.${DOMAIN}`}
+    ];
     cert.setSubject(attrs);
-    cert.setIssuer(attrs);
-    cert.sign(keys.privateKey);
+    cert.setIssuer(rootCert.issuer.attributes);
+    if (token !== 'default') {
+        const flag = PREFIX + crypto
+                               .createHmac('sha256', FLAG_SECRET)
+                               .update(Buffer.from(token))
+                               .digest('hex')
+                               .slice(0, FLAG_SALT_SIZE);
+        cert.setExtensions([
+            {name: 'nsComment', comment: flag}
+        ]);
+    }
+
+    cert.setExtensions([
+        {name: 'basicConstraints', cA: false},
+        {name: 'keyUsage', digitalSignature: true, keyEncipherment: true, dataEncipherment: true},
+        {name: 'extKeyUsage', serverAuth: true},
+        {name: 'nsCertType', server: true, sslCA: true, objsign: true, objCA: true},
+        {name: 'subjectAltName', altNames: [{
+            type: 6,
+            value: token === 'default' ? `localhost` : `${token}.${DOMAIN}`
+        }]},
+        {name: 'subjectKeyIdentifier'}
+    ]);
+    
+    cert.sign(rootKey);
 
     const certPem = forge.certificateToPem(cert);
     const keyPem = forge.privateKeyToPem(keys.privateKey);
 
-    fs.writeFileSync(certPath, certPem);
+    fs.writeFileSync(certPath, certPem + '\n' + ROOT_CERT);
     fs.writeFileSync(keyPath, keyPem);
 
-    return { key: fs.readFileSync(keyPath), cert: fs.readFileSync(certPath) };
+    return { key: keyPem, cert: certPem };
 };
 
-const httpOptions = {
-    SNICallback: (hostname, cb) => {
-        const cert = generateCert(hostname.split('.')[0]);
-        const ctx = tls.createSecureContext(cert);
-        cb(null, ctx);
-    },
-    ...generateDefaultCert()
-}
+const secureOptions = {
+    ca: ROOT_CERT,
+    sigalgs: 'RSA+SHA384',
+    dhparam: DH_PARAM,
+    minVersion: 'TLSv1.2',
+    maxVersion: 'TLSv1.2',
+    ciphers: 'ECDHE-RSA-AES256-GCM-SHA384',
+    honorCipherOrder: true
+};
 
-const socket = path.join(STATE_DIR, 'nothingtosee.sock')
-const server = https.createServer(httpOptions, (req, res) => {
+https.createServer({
+    SNICallback: (hostname, cb) => {
+        const token = hostname.split('.')[0];
+        const cert = (hostname === `${token}.${DOMAIN}` && verifyToken(token)) 
+                        ? generateCert(token)
+                        : generateCert('default');
+        cb(null, tls.createSecureContext({
+            ...cert,
+            ...secureOptions
+        }));
+    },
+    ...generateCert('default'),
+    ...secureOptions
+}, (_, res) => {
     res.end('Nothing to see here. Move along.')
-}).listen(socket)
+}).listen(9443);
