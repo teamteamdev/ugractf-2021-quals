ce82a79b785c4085fe122a73222dc8ea4c123177
Nikolay Amiantov <ab@fmap.me>
2021-02-28 17:37:42 +0300

    Add logs for compliance

--- tasks/compliance/app/start_server.sh
+++ tasks/compliance/app/start_server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-export SOCK_PATH=$1/sock
+export SOCK_PATH=$1
 mkdir $SOCK_PATH/store
 chmod 777 $SOCK_PATH/store
 exec docker-compose -p "$NEW_UUID" up
